8708e13697d6328ef6f900376b107aac4edde105
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2020-06-10 14:04:09 -0700

    Add files via upload

--- RTKsurvey/RTKsurvey.ino
+++ RTKsurvey/RTKsurvey.ino
@@ -11,31 +11,20 @@
     Once we've achieved 2m accuracy and 300s have passed, survey is complete
     Enable four RTCM messages
     Begin outputting RTCM bytes
-
-  Feel like supporting open source hardware?
-  Buy a board from SparkFun!
-  ZED-F9P RTK2: https://www.sparkfun.com/products/15136
-  NEO-M8P RTK: https://www.sparkfun.com/products/15005
-  SAM-M8Q: https://www.sparkfun.com/products/15106
-
-  Hardware Connections:
-  Plug a Qwiic cable into the GPS and a BlackBoard
-  Plug a SerLCD onto the Qwiic bus
-  If you don't have a platform with a Qwiic connection use the SparkFun Qwiic Breadboard Jumper (https://www.sparkfun.com/products/14425)
-  Watch the output on the LCD or open the serial monitor at 115200 baud to see the output
 */
 
 #define STAT_LED 13
 
 #include <Wire.h> //Needed for I2C to GPS
 
-#include "SparkFun_Ublox_Arduino_Library.h" //Click here to get the library: http://librarymanager/All#SparkFun_Ublox_GPS
+#include "SparkFun_Ublox_Arduino_Library.h"
 SFE_UBLOX_GPS myGPS;
 
-#include <Adafruit_LiquidCrystal.h> //Click here to get the library: http://librarymanager/All#SparkFun_SerLCD
+#include <Adafruit_LiquidCrystal.h>
 Adafruit_LiquidCrystal lcd(0);
-void setup()
-{
+
+bcount = 0;		// RTCM message counter
+void setup() {
   Serial.begin(115200);
   while (!Serial); //Wait for user to open terminal
   Serial.println("Ublox GPS I2C Test");
@@ -51,13 +40,12 @@ void setup()
   delay(5000);
 
   myGPS.begin(Wire);
-  if (myGPS.isConnected() == false)
-  {
+  if (myGPS.isConnected() == false) {
     Serial.println(F("Ublox GPS not detected at default I2C address. Please check wiring. Freezing."));
     lcd.setCursor(0, 1);
     lcd.print(F("No GPS detected"));
     while (1);
-  }
+	}
 
   Wire.setClock(400000); //Increase I2C clock speed to 400kHz
 
@@ -67,31 +55,27 @@ void setup()
   //Check if Survey is in Progress before initiating one
   boolean response;
   response = myGPS.getSurveyStatus(2000); //Query module for SVIN status with 2000ms timeout (request can take a long time)
-  if (response == false)
-  {
+  if (response == false) {
     Serial.println(F("Failed to get Survey In status"));
     while (1); //Freeze
-  }
+	}
 
-  if (myGPS.svin.active == true)
-  {
+  if (myGPS.svin.active == true) {
     Serial.print(F("Survey already in progress."));
     lcd.setCursor(0, 2);
     lcd.print(F("Survey already going"));
-  }
-  else
-  {
+	}
+  else {
     //Start survey
     response = myGPS.enableSurveyMode(300, 2.000); //Enable Survey in, 300 seconds, 2.0m
-    if (response == false)
-    {
+    if (response == false) {
       Serial.println(F("Survey start failed"));
       lcd.setCursor(0, 3);
       lcd.print(F("Survey start failed"));
       while (1);
-    }
+	  }
     Serial.println(F("Survey started. This will run until 300s has passed and less than 2m accuracy is achieved."));
-  }
+	}
 
   while (Serial.available()) Serial.read(); //Clear buffer
 
@@ -99,23 +83,19 @@ void setup()
   lcd.print(F("Survey in progress"));
 
   //Begin waiting for survey to complete
-  while (myGPS.svin.valid == false)
-  {
-    if (Serial.available())
-    {
+  while (myGPS.svin.valid == false) {
+    if (Serial.available()) {
       byte incoming = Serial.read();
-      if (incoming == 'x')
-      {
+      if (incoming == 'x') {
         //Stop survey mode
         response = myGPS.disableSurveyMode(); //Disable survey
         Serial.println(F("Survey stopped"));
         break;
-      }
-    }
+		}
+	 }
 
     response = myGPS.getSurveyStatus(2000); //Query module for SVIN status with 2000ms timeout (req can take a long time)
-    if (response == true)
-    {
+    if (response == true) {
       Serial.print(F("Press x to end survey - "));
       Serial.print(F("Time elapsed: "));
       Serial.print((String)myGPS.svin.observationTime);
@@ -131,14 +111,13 @@ void setup()
       lcd.setCursor(0, 2);
       lcd.print(F("Accuracy: "));
       lcd.print((String)myGPS.svin.meanAccuracy);
-    }
-    else
-    {
+	  }
+    else {
       Serial.println(F("SVIN request failed"));
-    }
+	  }
 
     delay(1000);
-  }
+	}
   Serial.println(F("Survey valid!"));
 
 // ------------------------------------------------------------------------------------- 
@@ -150,31 +129,26 @@ void setup()
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1124, COM_PORT_I2C, 1);
   response &= myGPS.enableRTCMmessage(UBX_RTCM_1230, COM_PORT_I2C, 10); //Enable message every 10 seconds
 
-  if (response == true)
-  {
+  if (response == true) {
     Serial.println(F("RTCM messages enabled"));
-  }
-  else
-  {
+	}
+  else {
     Serial.println(F("RTCM failed to enable. Are you sure you have an NEO-M8P?"));
     while (1); //Freeze
-  }
+	}
 
   Serial.println(F("Base survey complete! RTCM now broadcasting."));
   myGPS.setI2COutput(COM_TYPE_UBX | COM_TYPE_RTCM3);
   lcd.clear();
   lcd.print(F("Transmitting RTCM"));
   
-}
+  }
 
-void loop()
-{
+void loop() {
   myGPS.checkUblox(); //See if new data is available. Process bytes as they come in.
-
-  //Do anything you want. Call checkUblox() every second. NEO-M8P-2 has TX buffer of 4k bytes.
   
   delay(250); //Don't pound too hard on the I2C bus
-}
+  }
 
 //This function gets called from the SparkFun Ublox Arduino Library.
 //As each RTCM byte comes in you can specify what to do with it
@@ -182,8 +156,10 @@ void loop()
 void SFE_UBLOX_GPS::processRTCM(uint8_t incoming)
 {
   //Let's just pretty-print the HEX values for now
-  if (myGPS.rtcmFrameCounter % 16 == 0) Serial.println();
+  if (bcount % 16 == 0) Serial.println();
   Serial.print(" ");
   if (incoming < 0x10) Serial.print("0");
   Serial.print(incoming, HEX);
+  bcount++;
+
 }
