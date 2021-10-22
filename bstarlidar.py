6bf811daa19f370e93b5168253646883bf282015
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2021-10-22 12:26:19 -0700

    Add files via upload

--- bstarlidar.py
+++ bstarlidar.py
@@ -113,53 +113,6 @@ routes = [[0,0],                    #0
     [0]]
           
 wptdist = 0.0
-
-# (x, y) ((East, North)) in U.S. Survey feet offsets from 34-14N -119-4W
-'''
-waypts=[[0,1],[1,2],[2,3],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9],[9,10],
-    [ -787.36,  2298.03],     #10 
-    [ -787.00,  1977.00],     #11 Arena pepper
-    [ -758.44,  1910.67],     #12 Arena spur
-    [ -599.97,  2236.32],     #13 workshop F 
-    [ -578.94,  2247.67],     #14 driveway center F
-    [ -490.20,  2097.75],     #15 gravel rev 210430
-    [ -471.80,  2053.82],     #16 fig tree fork F
-    [ -698.89,  1856.89],     #17 Arena throat
-    [ -592.93,  1931.82],     #18 shed #3/#4 F
-    [ -526.33,  1863.82],     #19 longe center
-    [ -661.79,  1842.34],     #20 stall ctr
-    [ -511.84,  2145.63],     #21 E dway start
-    [ -548.45,  1951.78],     #22 hut row bend F
-    [ -619.07,  2315.06],     #23 trash
-    [ -482.00,  1902.22],     #24 Arena ease exit
-    [ -665.89,  2108.14],     #25 ref corner - F
-    [ -464.67,  1928.44],     #26 Arena turnoff
-    [ -640.51,  2179.75],     #27 rose bush - F 210102 modified to avoid roses
-    [ -624.85,  2235.41],     #28 boat corner - F 201230 - refounded
-    [ -684.91,  2276.04],     #29 EF middle - F
-    [ -644.70,  2261.65],     #30 office gap
-    [ -653.41,  2229.63],     #31 EF rose gap
-    [ -718.05,  2274.10],     #32 To main gate
-    [ -775.75,  2299.39],     #33 To main gate
-    [ -809.37,  2295.23],     #34 W of driveway
-    [ -844.68,  2286.36],     #35 g gate
-    [ -814.28,  2152.33],     #36 Down ag road
-    [ -784.66,  2132.42],     #37 Horse gar entrance
-    [ -725.68,  2148.27],     #38 Above horse gar
-    [ -694.38,  2162.42],     #39 Drive way above gar
-    [ -686.08,  2149.59],     #40 EF entrance
-    [ -662.14,  2171.71],     #41 Into EF
-    [ -662.99,  2114.42],     #42 Potty turn
-    [ -648.05,  2126.97],     #43 Potty pass
-    [ -672.13,  2294.55],     #44 EF mid to back gate
-    [ -677.84,  2330.52],     #45 EF to back gate
-    [ -663.71,  2339.98],     #46 EF to back gate
-    [ -641.60,  2338.68],     #47 To Back gate driveway
-    [ -634.85,  2340.83],     #48 Back drive driveway
-    [ -608.32,  2295.08],     #49 Back gate t\into EF
-    [ -647.22,  2276.52],     #50 into EF
-    [  -0.00,  0.00]]
-'''
 ndx = 0
 
 version = "Rover 1.1 211020\n"
@@ -182,37 +135,6 @@ tty = serial.Serial(port, 9600)
 tty.flushInput()
 
 #====================================================
-'''
-def cartesian(compass):
-    return (450 - compass) % 360
-def vadd(U, V):
-    return [U[0]+V[0], U[1]+V[1]]
-def vcross2(U, V):
-    x = U[0] * V[1] - U[1] * V[0]
-    if x > 0:
-        return True      # U x V right hand rotation
-    return False
-def vdot(U, V):
-    return (U[0]*V[0] + U[1]*V[1])
-def vmag(V):
-    return math.sqrt(V[0]*V[0] + V[1]*V[1])
-def vsmult(V, scalar):
-    return [V[0]*scalar, V[1]*scalar]
-def vsub(headV, tailV):
-    return [headV[0]-tailV[0], headV[1]-tailV[1]]
-def vunit(V):
-    mag = vmag(V)
-    return [V[0]/mag, V[1]/mag]
-
-# get compass course from direction vector
-def vcourse(V):
-    return (450 - math.degrees(math.atan2(V[1],V[0]))) % 360
-# get unit vector from compass heading
-def vcompass(angle):
-    cart = cartesian(angle)
-    rcart = math.radians(cart)
-    return [math.cos(rcart), math.sin(rcart)]
-'''
 # cvt lat/lon seconds to U.S survey feet
 def vft2sec(feetE, feetN):
     return [feetN/latfeet, feetE/lonfeet]
