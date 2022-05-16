cfbe77f4562fdf007b3668fbd375cfdd66a37927
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2022-05-16 11:27:54 -0700

    Add files via upload

--- whichrover.py
+++ whichrover.py
@@ -1,9 +1,9 @@
 # definitions for blue rover
 Rcompass_bias = 98          # for canopy table (using gyro/quat, no-mag)
-Rrrbias = 64
-Rrfbias = 69
-Rlfbias = 44
-Rlrbias = 61
+Rrrbias = 46
+Rrfbias = 62
+Rlfbias = 74
+Rlrbias = 59
 
 Rpanbias = 83
 
@@ -11,4 +11,4 @@ Rd1 = 7.254         #C/L to corner wheels
 Rd2 = 10.5          #mid axle to fwd axle
 Rd3 = 10.5          #mid axle to rear axle
 Rd4 = 10.073        #C/L to mid wheels
-Rspeedfactor = 44   # 4000 counts at 100%
+Rspeedfactor = 40   # 4000 counts at 100%
