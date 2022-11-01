adf001c991fcfafe176728213e1a44a868e241f5
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2022-11-01 15:22:51 -0700

    Add files via upload

--- linedist.py
+++ linedist.py
@@ -1,10 +1,16 @@
 from vectors import *
+'''
 a0 = [3.0, 0.0]
 a1 = [9.0, 0.0]
 b0 = [5.0, 3.0]
 #b0 = [0.0, 3.0]
 #b1 = [8.0, 3.0]
 b1 = [8.0, 8.0]
+'''
+a0 = [-593.08, 2132.60]
+a1 = [-576.59, 2136.79]
+b0 = [-598.46, 2124.46]
+b1 = [-577.12, 2133.53]
 
 v = vsub(a1, a0)
 u = vsub(b1, b0)
@@ -15,25 +21,38 @@ print("u", u)
 print("w", w)
 print("x", x)
 wv = vdot(w, v)
-vx = -vdot(v, x)
+vx = -vdot(v, x)   # reverse direction of v
 print(wv, vx)
-if wv < 0 and vx < 0:      # both angles obtuse
-    dist1 = pldistance(b1, a0, a1)
-    dist2 = pldistance(b0, a0, a1)
-    dist = min(dist1, dist2)
-    print("line b outside line a", dist)
-if wv > 0 and vx > 0:      # both angles acute
-    dist1 = pldistance(a0, b1, b0)
-    dist2 = pldistance(a1, b1, b0)
-    dist = min(dist1, dist2)
-    print("line b inside line a", dist1, dist2, dist)
-if wv > 0 and vx < 0:
+if wv <= 0 and vx <= 0:      # case 1 a line within b line
+    dist1 = pldistance(a0, b0, b1)
+    dist2 = pldistance(a1, b0, b1)
+    print("line b outside line a", dist1, dist2)
+    perp = vperp(u)
+elif wv > 0 and vx > 0:      # case 2 b line within a line
+    dist1 = pldistance(b0, a0, a1)
+    dist2 = pldistance(b1, a0, a1)
+    print("line b inside line a", dist1, dist2)
+    perp = vperp(v)
+elif wv > 0 and vx < 0:      # case 3 offset lines right
     dist1 = pldistance(b0, a0, a1)
     dist2 = pldistance(a1, b0, b1)
-    dist = min(dist1, dist2)
-if wv < 0 and vx > 0:
+    print("line b alongside line a", dist1, dist2)
+    if vmag(v) > vmag(u):
+        perp = vperp(v)
+    else:
+        perp = vperp(u)
+else:                        # case 4 offset lines left
     dist1 = pldistance(b1, a0, a1)
     dist2 = pldistance(a0, b0, b1)
-    dist = min(dist1, dist2)
+    print("line b beside line a", dist1, dist2)
+    if vmag(v) > vmag(u):
+        perp = vperp(v)
+    else:
+        perp = vperp(u)
 
-print(dist)
+    
+dist = min(dist1, dist2)
+
+print(1/dist, vunit(perp))
+print(vcourse(perp))
+
